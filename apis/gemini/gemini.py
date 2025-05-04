9387a16870be3ee84a0aa2e80b31aecf27e5ffb9
YangchichengShen <78356476+YangchichengShen@users.noreply.github.com>
2025-05-04 16:55:01 -0500

    ✨ Add gemini.py to implement content generation using Gemini API

---
+++ apis/gemini/gemini.py
@@ -0,0 +1,20 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+key = os.getenv("GEMINI_KEY")
+
+client = genai.Client(api_key=key)
+
+def call_gemini_2_flash(prompt, temperature=0.5):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        # Output a list of the users interests
+        contents=prompt,
+        config={"temperature": 0.0},
+    )
+    return response.text
+
+# # Test the function with a sample prompt
+# print(call_gemini_2_flash("Hello"))
